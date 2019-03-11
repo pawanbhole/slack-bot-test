afe2d18fa323e0099ca5a5e89034d10a44a6e433
Pawan Bhole <pawan@Pawans-MacBook-Air.local>
2019-03-11 10:07:28 +0530

    cleanup

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import SlackBot from './components/SlackBot';
 import Logger from './components/Logger';
 import I18n from './components/I18n';
-require('./ErrorToJSONPolyfill');
+require('./components/ErrorToJSONPolyfill');
 
 const logger = new Logger();
 const i18n = new I18n();
